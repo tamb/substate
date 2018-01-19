c2605d679426336e7f8b4f971ab611f3d3dcd3cb
TomSaporito <t.saporito@gmail.com>
2018-01-18 23:28:07 -0500

    index is at root

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
     },
 
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(__dirname, '/'),
         filename: 'index.js',
         library: 'SubState',
         libraryExport: "default"
