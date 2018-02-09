ac8d7df31f4e7ba81d828bcb8c2b74735c9dba32
TomSaporito <t.saporito@gmail.com>
2018-02-09 15:56:35 -0500

    fixed .getProp method

--- src/parts/SubState.prod.js
+++ src/parts/SubState.prod.js
@@ -69,7 +69,7 @@ export default class SubState extends PubSub {
 
     getProp(prop) {
         //TODO does not work need to rewrite since object.bystring is rewritten
-        return Object.byString(this.getCurrentState(), prop);
+        return this.getCurrentState().byString( prop);
     }
 
 
