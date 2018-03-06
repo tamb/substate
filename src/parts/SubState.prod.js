5aa270533d07721d095f038e76e3f7867fe6b774
TomSaporito <t.saporito@gmail.com>
2018-03-06 12:09:36 -0500

    used mdn Object.assign polyfill

--- src/parts/SubState.prod.js
+++ src/parts/SubState.prod.js
@@ -1,31 +1,29 @@
+if (typeof Object.assign != 'function') {
+    Object.assign = function(target, varArgs) { // .length of function is 2
+        'use strict';
+        if (target == null) throw new TypeError('Cannot convert undefined or null to object');
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) { // Skip over if undefined or null
+                for (var nextKey in nextSource) {
+                    // Avoid bugs when hasOwnProperty is shadowed
+                    if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) to[nextKey] = nextSource[nextKey];
+                        
+                    
+                }
+            }
+        }
+        return to;
+    };
+}
+
 import bystring from 'object-bystring';
-const objectAssign = require('object-assign');
 import PubSub from './PubSub.js';
 
-// if (typeof Object.assign != 'function') {
-//     Object.assign = function(target, varArgs) { // .length of function is 2
-//         'use strict';
-//         if (target == null) { // TypeError if undefined or null
-//             throw new TypeError('Cannot convert undefined or null to object');
-//         }
 
-//         var to = Object(target);
-
-//         for (var index = 1; index < arguments.length; index++) {
-//             var nextSource = arguments[index];
-
-//             if (nextSource != null) { // Skip over if undefined or null
-//                 for (var nextKey in nextSource) {
-//                     // Avoid bugs when hasOwnProperty is shadowed
-//                     if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-//                         to[nextKey] = nextSource[nextKey];
-//                     }
-//                 }
-//             }
-//         }
-//         return to;
-//     };
-// }
 
 export default class SubState extends PubSub {
     constructor(objParam, inst) {
