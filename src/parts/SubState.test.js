0f1ae0b3186a6768a58c99dbbd2e9b7594b8cc34
tamb <t.saporito@gmail.com>
2019-09-19 18:40:19 -0400

    add tests and update readme

--- src/parts/SubState.test.js
+++ src/parts/SubState.test.js
@@ -1,5 +1,12 @@
 import substate from './SubState';
 
+const func1 =jest.fn(x => {
+    x.count? x.count = ++x.count : x.count = 1;
+});
+const func2 =jest.fn(x => {
+    x.count2? ++x.count2 : x.count2 = 1;
+});
+
 const NAME = 'MyName';
 const STATE = {
     name: 'Thomas',
@@ -12,8 +19,12 @@ const STATE = {
 };
 
 const A = new substate({
+    name: 'HamburgerStore',
     defaultDeep: true,
-    state: STATE
+    state: STATE,
+    beforeUpdate: func1,
+    afterUpdate: func2
+
 });
 
 const B = new substate({
@@ -26,6 +37,10 @@ test('creates new instance of substate', ()=>{
     expect(A instanceof substate).toBe(true);
 });
 
+test('expects store to have name', ()=>{
+    expect(A.name).toBe('HamburgerStore');
+})
+
 test('events to contain UPDATE_STATE on initialization', ()=>{
     expect(A.events.UPDATE_STATE).toHaveLength(1);
 });
@@ -34,9 +49,13 @@ test('get props to return correct value', ()=>{
     expect(A.getProp('nested.double.reason')).toBe('Just the start');
 });
 
-test('getCurrentState returns current state', ()=>{
+
+
+test('getCurrentState returns current state and fires middleware', ()=>{
     expect(A.getCurrentState()).toMatchObject(STATE);
     A.emit('UPDATE_STATE', {timeOfFun: new Date()});
+    expect(A.count).toBe(1);
+    expect(A.count2).toBe(1);
     expect(A.getCurrentState()).not.toMatchObject(STATE);
 });
 
@@ -47,6 +66,8 @@ test('getState returns correct state from array', ()=>{
 test('deep clonse works and does not alter older nested state',()=>{
     const NEWTEXT = 'This has changed';
     A.emit('UPDATE_STATE', {'nested.double.reason': NEWTEXT});
+    expect(A.count).toBe(2);
+    expect(A.count2).toBe(2);
     expect(A.getState(0)).not.toBe(NEWTEXT);
 });
 
