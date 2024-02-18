5686e3403c395c6743b36016ab857d73ca2b1006
¡Támb! <t.saporito@gmail.com>
2024-02-17 22:37:02 -0500

    Uses new deep clone library (#69)

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
-import rfdc from "rfdc";
+import cloneDeep from "clone-deep";
 import byString from "object-bystring";
 
 import PubSub, { IPubSub } from "./PubSub";
 
 const S: string = "UPDATE_STATE";
-const C: string = "CHANGE_STATE";
 
-interface ISubstate extends IPubSub {
+interface IState {
+  [key: string]: any;
+  $type?: string;
+  $deep?: boolean;
+}
+
+export interface ISubstate extends IPubSub {
   name: string;
   afterUpdate: Function[] | [];
   beforeUpdate: Function[] | [];
   currentState: number;
-  stateStorage: object[];
+  stateStorage: IState[];
   defaultDeep: boolean;
   getState(index: number): {};
-  getCurrentState(): {};
+  getCurrentState(): IState;
   getProp(prop: string): any;
   resetState(): void;
-  updateState(action: IAction): void;
+  updateState(action: IState): void;
 }
 
-interface IAction extends Object {
-  $type?: string;
-  $deep?: boolean;
-  [key: string]: any;
-}
-
-interface IConfig {
+export interface IConfig {
   name?: string;
   afterUpdate?: Function[] | [];
   beforeUpdate?: Function[] | [];
@@ -36,11 +35,11 @@ interface IConfig {
   state?: object;
 }
 
-interface IChangeStateAction extends IAction {
+export interface IChangeStateAction extends IState {
   $requestedState: number;
 }
 
-export default class substate extends PubSub implements ISubstate {
+export default class Substate extends PubSub implements ISubstate {
   name: string;
   afterUpdate: Function[] | [];
   beforeUpdate: Function[] | [];
@@ -62,19 +61,19 @@ export default class substate extends PubSub implements ISubstate {
     this.on(S, this.updateState.bind(this));
   }
 
-  getState(index: number): {} {
+  public getState(index: number): {} {
     return this.stateStorage[index];
   }
 
-  getCurrentState(): {} {
+  public getCurrentState(): {} {
     return this.getState(this.currentState);
   }
 
-  getProp(prop: string): any {
+  public getProp(prop: string): any {
     return byString(this.getCurrentState(), prop);
   }
 
-  resetState() {
+  public resetState() {
     this.currentState = 0;
     this.stateStorage = [this.stateStorage[0]];
     this.emit("STATE_RESET");
@@ -86,33 +85,42 @@ export default class substate extends PubSub implements ISubstate {
     this.currentState = this.stateStorage.length - 1;
   }
 
-  updateState(action: IAction) {
+  private cloneState(deep: boolean): IState {
+    return deep
+      ? cloneDeep(this.getCurrentState())
+      : Object.assign({}, this.getCurrentState());
+  }
+
+  private fireBeforeMiddleware(action: IState): void {
     this.beforeUpdate.length > 0
       ? this.beforeUpdate.forEach((func) => func(this, action))
       : null;
-    let newState: IAction;
-    if (action.$deep || this.defaultDeep) {
-      newState = rfdc(this.getCurrentState()); // deep clone
-    } else {
-      newState = Object.assign({}, this.getCurrentState()); // shallow clone
-    }
+  }
+
+  private fireAfterMiddleware(action: IState): void {
+    this.afterUpdate.length > 0
+      ? this.afterUpdate.forEach((func) => func(this, action))
+      : null;
+  }
+
+  public updateState(action: IState): void {
+    this.fireBeforeMiddleware(action);
+    const deep: boolean =
+      action.$deep !== undefined ? action.$deep : this.defaultDeep;
+    const newState = this.cloneState(deep);
 
     //update temp new state
     for (let key in action) {
-      if (action.hasOwnProperty(key)) byString(newState, key, action[key]);
+      byString(newState, key, action[key]);
       //update cloned state
     }
 
     this.defaultDeep ? null : (newState.$deep = false); // reset $deep keyword
-
-    if (!action.$type) newState.$type = S;
+    newState.$type = action.$type || S; // set $type if not already set
 
     //pushes new state
     this.pushState(newState);
-
-    this.afterUpdate.length > 0
-      ? this.afterUpdate.forEach((func) => func(this))
-      : null;
+    this.fireAfterMiddleware(action);
     this.emit(action.$type || "STATE_UPDATED", this.getCurrentState()); //emit with latest data
   }
 }
